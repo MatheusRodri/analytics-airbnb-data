6cff3be54d4626c1ce9f3d01be98735be6299cd0
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2026-02-18 18:02:28 -0300

    Update dash_airbnb.py

--- dash_airbnb.py
+++ dash_airbnb.py
@@ -19,4 +19,8 @@ def get_pyg_renderer(dataframe):
     return StreamlitRenderer(dataframe, spec="streamlit_config.json", spec_io_mode="rw")
 
 renderer = get_pyg_renderer(df_rio)
-renderer.viewer()
+
+col1, col2, col3 = st.columns([1, 4, 1])
+
+with col2:
+    renderer.viewer()
